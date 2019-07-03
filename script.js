1fd83298cabe1f94979be10e4b93c1770174a1dd
LazarievEd <lazarev223@gmail.com>
2019-07-03 20:32:09 +0300

    add dz10

---
+++ script.js
@@ -0,0 +1,30 @@
+var first_num = +prompt('Введите число - которое быдем возводить в степень:');
+var second_num = +prompt('Введите степень, в которую будем возводить число:');
+var def = 1;
+
+for (i = second_num; i; i--) {
+    def *= first_num;
+}
+
+alert ('"for" ответ: ' + def);
+
+def = 1;
+var whi_i = 0;
+
+while (whi_i < second_num) {
+  def *= first_num;
+  whi_i++;
+}
+
+alert ('"while" ответ: ' + def);
+
+daf = 1;
+var clean = 1;
+var do_i = 0;
+do {
+  def *= clean;
+  clean = first_num;
+  do_i--;
+} while (do_i + 1)
+
+alert ('"do...while" ответ: ' + def);
